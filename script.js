aa6d13d90afd702828446cd73efdd6f1c65ff3aa
bipin710 <chaulagainumesh4@gmail.com>
2025-06-04 15:43:33 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navigation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+  });
+});
